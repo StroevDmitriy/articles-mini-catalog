c9442d40efcdf646573d27413f3cb2f91acb0be0
Stroev D <stroev.core@gmail.com>
2023-06-25 00:59:21 +0300

    added ArticleCard component

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,49 @@
     </header>
     <main>
       <section class="articles-list">
+        <ArticleCard 
+          previewName="article-image"
+        />
+        <ArticleCard 
+          previewName="article-image"
+        />
+        <ArticleCard 
+          previewName="article-image"
+        />
+        <ArticleCard 
+          previewName="article-image"
+        />
+        <ArticleCard 
+          previewName="article-image"
+        />
+        <ArticleCard 
+          previewName="article-image"
+        />
+        <ArticleCard 
+          previewName="article-image"
+        />
+      </section>
+
+      <!-- <section class="articles-list">
         <strong>Пока нет статей</strong>
         <p>Создайте свою первую  категорию и добавьте в неё статьи.</p>
         <CreateCategoryButton />
-      </section>
+      </section> -->
     </main>
   </div>
 </template>
 
 <script>
 import ToolBar from './components/ToolBar.vue';
-import CreateCategoryButton from './components/CreateCategoryButton.vue';
+// import CreateCategoryButton from './components/CreateCategoryButton.vue';
+import ArticleCard from './components/ArticleCard.vue'
 
 export default {
   name: 'App',
   components: {
     ToolBar,
-    CreateCategoryButton
+    ArticleCard
+    // CreateCategoryButton
   }
 }
 </script>
@@ -30,11 +56,10 @@ export default {
 .articles-list {
   display: flex;
   margin: 144px auto 0;
-  flex-direction: column;
+  flex-wrap: wrap;
   align-items: center;
-  width: 260px;
+  gap: 24px;
   font-size: 14px;
-  text-align: center;
 }
 
 .articles-list strong {
--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -1,6 +1,26 @@
 <template>
-  <article>
-
+  <article class="article-card">
+    <div class="article-card__interaction-row">
+      <button
+        type="button"
+        class="article-card__like-button"
+      >
+        <img src="../assets/svg/like_default.svg" alt="Like">
+        <!-- <img src="../assets/svg/like_active.svg" alt="Like"> -->
+        40
+      </button>
+      <button
+        type="button"
+        class="article-card__edit-button"
+      >
+        <img src="../assets/svg/pen.svg" alt="Like">
+      </button>
+    </div>
+    <div class="article-card__preview-container">
+      <img :src="require(`@/assets/images/${previewName}.png`)"  alt="Article preview"/>
+    </div>
+    <h4 class="article-card__title">Заголовок</h4>
+    <p class="article-card__description">Идейные соображения высшего порядка, а также сложившаяся структура организации играет важную роль в формировании существенных финансовых и административных условий. Равным образом начало повседневной работы по формированию позиции представляет собой интересный эксперимент проверки направлений прогрессивного развития.</p>
   </article>
 </template>
 
@@ -8,6 +28,12 @@
 export default {
   name: "article-card",
   components: {},
+  props: {
+    previewName: {
+      type: String,
+      default: ""
+    }
+  },
   data() {
     return {};
   },
@@ -15,4 +41,56 @@ export default {
 
 </script>
 <style scoped>
+.article-card {
+  width: 291px;
+  box-sizing: border-box;
+  height: 295px;
+  border: 1px solid #BFC3D5;
+  border-radius: 10px;
+  padding: 15px;
+}
+
+.article-card__interaction-row {
+  display: flex;
+  margin-bottom: 8px;
+  justify-content: space-between;
+}
+
+.article-card__like-button {
+  align-items: center;
+  color: #A0A6BF;
+  font-size: 12px;
+}
+
+.article-card__edit-button {
+  border: none
+}
+
+.article-card__preview-container {
+  width: 100%;
+  height: 80px;
+  margin-bottom: 8px;
+}
+
+.article-card__preview-container img {
+  object-fit: cover;
+  height: 100%;
+  width: 100%;
+}
+
+.article-card__title {
+  font-size: 16px;
+  font-weight: 800;
+  line-height: 1.5;
+  margin-bottom: 8px;
+}
+
+.article-card__description {
+  display: -webkit-box;
+  overflow: hidden;
+  -webkit-line-clamp: 5;
+  -webkit-box-orient: vertical;
+  color: #6A6E7E;  
+  line-height: 22px;
+}
 </style>
