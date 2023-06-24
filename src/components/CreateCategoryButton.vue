406dbf677f2d196c034c118947efff2f7ee1102e
Stroev D <stroev.core@gmail.com>
2023-06-24 16:35:22 +0300

    created CustomButton and CreateCategoryButton components, added button to the ToolBar

---
+++ src/components/CreateCategoryButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <CustomButton
+    button-label="Создать категорию"
+    button-type="action"
+    class="create-category-button"
+  />
+</template>
+
+<script>
+import CustomButton from "./UI/CustomButton.vue"
+
+export default {
+  name: "create-category-button",
+  components: {
+    CustomButton,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+
+.create-category-button {
+  position: relative;
+  margin-right: 16px;
+  padding-right: 48px;
+}
+
+.create-category-button::after {
+  position: absolute;
+  content: '+';
+  top: 50%;
+  transform: translateY(calc(-50% - 2px));
+  margin-left: 13px;
+  font-size: 30px;
+  font-weight: 100;
+}
+</style>
---
+++ src/components/UI/CustomButton.vue
@@ -0,0 +1,87 @@
+<template>
+  <button
+    type="button"
+    :class="[
+      'custom-button',
+      {
+        'action-button': buttonType === 'action',
+        'passive-button': buttonType === 'passive',
+        'inactive-button': buttonType === 'inactive',
+      },
+    ]"
+  >{{ buttonLabel }} </button>
+</template>
+
+<script>
+export default {
+  name: "custom-button",
+  components: {},
+  props: {
+    buttonLabel: {
+      default: 'Текст кнопки',
+      type: String
+    },
+    buttonType: {
+      default: 'passive',
+      type: String,
+      validator(value) {
+        return ['action', 'passive'].includes(value)
+      }
+    }
+  },
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+};
+
+</script>
+
+<style scoped>
+.custom-button{
+  margin-right: 16px;
+  border: none;
+  border-radius: 3px;
+  padding: 10px 21px 12px 16px;
+  
+  font-size: 16px;
+  line-height: 1.5;
+  transition: background-color .3s;
+  cursor: pointer;
+}
+.action-button {
+  color: #fff;
+  background-color: #ED5252;
+}
+
+.action-button:hover {
+  background-color: #F06C6C;
+}
+
+.action-button:active {
+  background-color: #D54A4A;
+}
+
+.action-button:disabled {
+  background-color: #F0F1F2;
+}
+
+.passive-button {
+  color: #303446;
+  background-color: rgba(10, 14, 32, 0.07);
+}
+
+.passive-button:hover {
+  background-color: rgba(10, 14, 32, 0.1);
+}
+
+.passive-button:active {
+  background-color: rgba(10, 14, 32, 0.15);
+}
+
+.passive-button:disabled {
+  color: #fff;
+  background-color: rgba(197, 199, 208, 1);
+}
+</style>
