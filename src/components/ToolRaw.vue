55e4b7ab31d8fb5a289a52ebca88073bdff34938
Stroev D <stroev.core@gmail.com>
2023-06-24 15:08:15 +0300

    initial layout and styles set, ToolRaw component created

---
+++ src/components/ToolRaw.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tool-raw">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tool-raw",
+  components: {},
+};
+</script>
+<style scoped>
+.tool-raw {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
