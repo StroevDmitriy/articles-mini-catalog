b4869d9a351888a8e73d210588a75df90d8a2d61
Stroev D <stroev.core@gmail.com>
2023-06-25 00:57:11 +0300

    svg rename

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <article>
+
+  </article>
+</template>
+
+<script>
+export default {
+  name: "article-card",
+  components: {},
+  data() {
+    return {};
+  },
+};
+
+</script>
+<style scoped>
+</style>
